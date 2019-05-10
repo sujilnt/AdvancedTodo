855295c1d134240c7d846c264a645a212200cb0f
sujilnt <sujilnt@gmail.com>
2019-05-10 15:10:58 +0100

    Working of Webpack successful for react

---
+++ src/Components/App/App.js
@@ -0,0 +1,5 @@
+import React from "react";
+const App = ()=>{
+    return(<div>The is a Root App Component Sujil</div>);
+};
+export default App;
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,33 @@
-const path = require('path');
+const  path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports={
     entry:"./index.js",
     output:{
         filename: "main.js",
-        path: path.resolve(__dirname,'dist')
-    }
+        path: path.resolve(__dirname,"dist"),
+    },
+    devServer:{
+        compress: true,
+        writeToDisk: true
+    },
+    resolve: {
+        modules: [path.resolve(__dirname, 'src'), 'node_modules']
+      },
+    module:{
+        rules:[
+            {
+                test: /\.(js|jsx)$/,
+                exclude:/node_modules/,
+                use:{
+                    loader: "babel-loader"
+                }
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+          template: path.resolve("./index.html"),
+        })
+      ]
 };
