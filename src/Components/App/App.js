eceee5b6a5d23364966925c2e82fe6bfa5bf0dde
sujilnt <sujilnt@gmail.com>
2019-05-10 18:16:26 +0100

    Adding url-loader Need to add Optimization in webpack Added Css loader and tested in React EOF

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,5 +1,12 @@
-import React from "react";
-const App = ()=>{
-    return(<div>The is a Root App Component Sujil</div>);
+import React from 'react';
+import './App.css';
+import img from '../../../assets/images.png';
+const App = () => {
+	return (
+		<div className="container">
+			<div>The is a Root App Component Sujil</div>
+			<img src={img} />
+		</div>
+	);
 };
 export default App;
--- webpack.config.js
+++ webpack.config.js
@@ -1,33 +1,44 @@
-const  path = require('path');
+const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-
-module.exports={
-    entry:"./index.js",
-    output:{
-        filename: "main.js",
-        path: path.resolve(__dirname,"dist"),
-    },
-    devServer:{
-        compress: true,
-        writeToDisk: true
-    },
-    resolve: {
-        modules: [path.resolve(__dirname, 'src'), 'node_modules']
-      },
-    module:{
-        rules:[
-            {
-                test: /\.(js|jsx)$/,
-                exclude:/node_modules/,
-                use:{
-                    loader: "babel-loader"
-                }
-            }
-        ]
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-          template: path.resolve("./index.html"),
-        })
-      ]
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+module.exports = {
+	entry: './index.js',
+	output: {
+		filename: 'main.js',
+		path: path.resolve(__dirname, 'dist')
+	},
+	devServer: {
+		compress: true,
+		writeToDisk: true
+	},
+	resolve: {
+		modules: [ path.resolve(__dirname, 'src'), 'node_modules' ]
+	},
+	module: {
+		rules: [
+			{
+				test: /\.(js|jsx)$/,
+				exclude: /node_modules/,
+				use: {
+					loader: 'babel-loader'
+				}
+			},
+			{
+				test: /\.(css|scss)$/,
+				use: [ MiniCssExtractPlugin.loader, 'css-loader' ]
+			},
+			{
+				test: /\.(png|jpg|gif|jpe?g)$/i,
+				use: [
+					{
+						loader: 'url-loader',
+						options: {
+							limit: 8192
+						}
+					}
+				]
+			}
+		]
+	},
+	plugins: [ new HtmlWebpackPlugin({ template: path.resolve('./index.html') }), new MiniCssExtractPlugin() ]
 };
