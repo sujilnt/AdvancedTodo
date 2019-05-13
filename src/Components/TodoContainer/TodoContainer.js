02fa17a104b18d591ad059129eb286b7229d0f43
sujilnt <sujilnt@gmail.com>
2019-05-13 17:09:57 +0100

    adding some more changes

--- src/Components/TodoContainer/TodoContainer.js
+++ src/Components/TodoContainer/TodoContainer.js
@@ -1,101 +1,99 @@
 import React, { Component } from 'react';
 import './TodoContainer.css';
 import Navbar from '../NavBar/NavBar';
-import todoData from "../../data";
-import Section from "../Section/Section.js";
-import DisplayTasks from "../DisplayTasks/DisplayTasks.js"
-import _ from "../../Utils/utils.js";
+import todoData from '../../data';
+import Section from '../Section/Section.js';
+import DisplayTasks from '../DisplayTasks/DisplayTasks.js';
+import _ from '../../Utils/utils.js';
 class TodoContainer extends Component {
-	state={
-		data: todoData,
-		editedData:{},
-		currentTask:{},
-		currentId: ""
+	state = {
+		data: {},
+		editedData: {},
+		currentTask: {},
+		currentId: ''
 	};
 
 	// clearData - > clears the data.
-	clearData = (e)=>{
-		_.setItemInStorage("taskslist", {});
-		this.setState(()=>({
+	clearData = (e) => {
+		_.setItemInStorage('taskslist', {});
+		this.setState(() => ({
 			data: {}
 		}));
 	};
 
-	componentDidMount(){
-	  const tasklist = _.getItemStorage("taskslist");
-	  tasklist.length >0 ? this.setState(()=>({
-             data: tasklist
-	  })) : {};
+	componentDidMount() {
+		const tasklist = _.getItemStorage('taskslist');
+		console.log('t', tasklist);
 	}
 
 	// Onsave button functionality.
-	onSaveData = (currentData)=>{
-		console.log("current Data",this.state.data);
-		const letcurrentData = this.state.data.map((obj)=>{
-			return obj.id === currentData.id ?({
-					...obj,
-					name: currentData.name,
-					description: currentData.description
-			}) : obj;
+	onSaveData = (currentData) => {
+		console.log('current Data', this.state.data);
+		const letcurrentData = this.state.data.map((obj) => {
+			return obj.id === currentData.id
+				? {
+						...obj,
+						name: currentData.name,
+						description: currentData.description
+					}
+				: obj;
 		});
-		_.setItemInStorage("taskslist", letcurrentData);
-		console.log("called inside CurrenrData",letcurrentData);
-        this.setState(()=>({
+		_.setItemInStorage('taskslist', letcurrentData);
+		console.log('called inside CurrenrData', letcurrentData);
+		this.setState(() => ({
 			data: letcurrentData
 		}));
+	};
 
-	}
+	// add Todo Posts functionality
+	addDataFunction = (e) => {
+		console.log('currentData is ....', e);
+		this.setState((prev) => {
+			return this.state.data !== undefined && prev.data.length > 0
+				? { data: [ ...prev.data, e ] }
+				: { data: [ e ] };
+		});
+	};
 
-	// add Todo Posts functionality 
-	addDataFunction = (e)=>{
-		console.log("currentData is ....",e);
-		this.state.data === 0 ? this.setState((prev,props)=>({
-				data: [e]
-		})) 
-		:  this.setState((prev,props)=>({
-			data: [...prev.data,e]
-		}));
-	}
-
-	// generate tasks 
-	generateTasks = ()=>{
+	// generate tasks
+	generateTasks = () => {
 		const listofTasks = [];
-		this.state.data.map((obj)=>{
+		this.state.data.map((obj) => {
 			listofTasks.push(
-				 <DisplayTasks 
-					 name={obj.name} 
-					  description={obj.description}
-					  key={obj.key}
-					  data={obj}
-					  id={obj.id}
-					  onSave={this.onSaveData}
+				<DisplayTasks
+					name={obj.name}
+					description={obj.description}
+					key={obj.key}
+					data={obj}
+					id={obj.id}
+					onSave={this.onSaveData}
 				/>
-			)
+			);
 		});
 		return listofTasks;
-	}
-	render(){
-		const {data} = this.state;
+	};
+	render() {
+		const { data } = this.state;
 		const total = this.state.data.length;
 		//const incomplete = _.
-		console.log("currentState",this.state.data);
+		console.log('currentState', this.state.data);
 		return (
 			<div className="appContainer">
-				<Navbar  addTaskFunc={this.addDataFunction}/>	
+				<Navbar addTaskFunc={this.addDataFunction} />
 				<div className="todoBoard">
-				 <h2 className="h2style"> List of Todo Tasks... 
-				     <span className="clearTasks" onClick={this.clearData}>clear tasks</span>
-				 </h2>
-				 <hr className="hrstyle"/>
-				 <Section 
-				 		incomplete={2}
-						completed={3}
-						total={5}
-				 />
-				{ data.length > 0  ? this.generateTasks() : <div>Todo tasks need to added !</div>}
+					<h2 className="h2style">
+						{' '}
+						List of Todo Tasks...
+						<span className="clearTasks" onClick={this.clearData}>
+							clear tasks
+						</span>
+					</h2>
+					<hr className="hrstyle" />
+					<Section incomplete={2} completed={3} total={5} />
+					{data.length > 0 ? this.generateTasks() : <div>Todo tasks need to added !</div>}
 				</div>
 			</div>
 		);
 	}
-};
+}
 export default TodoContainer;
--- src/Utils/utils.js
+++ src/Utils/utils.js
@@ -1,39 +1,39 @@
-const dateCreation = ()=>{
-    const date = new Date().toString();
-    const boundary = date.search("GMT");
-    return date.substr(0,boundary).trim(); 
-}
-
-const mergeStateToObjects = (state, valueOne, valueTwo)=>{
-    const currentDateAndTime = dateCreation();
-  return {
-      id: geneateID(),
-      completed: false,
-      [valueOne]: state.taskName,
-      [valueTwo]: state.taskDescription,
-      created: currentDateAndTime
-  };
-}
-
-const lowerCaseFirstLetter= (letter)=>{
-   return letter.toString().charAt(0).toLowerCase() + letter.slice(1);
+const dateCreation = () => {
+	const date = new Date().toString();
+	const boundary = date.search('GMT');
+	return date.substr(0, boundary).trim();
 };
 
-const geneateID = ()=>{
-  return new Date().getTime().toString(36);
+const mergeStateToObjects = (state, valueOne, valueTwo) => {
+	const currentDateAndTime = dateCreation();
+	return {
+		id: geneateID(),
+		completed: false,
+		[valueOne]: state.taskName,
+		[valueTwo]: state.taskDescription,
+		created: currentDateAndTime
+	};
 };
 
-const setItemInStorage = (itemName, item)=>{
-  return localStorage.setItem(itemName,JSON.stringify(item));
+const lowerCaseFirstLetter = (letter) => {
+	return letter.toString().charAt(0).toLowerCase() + letter.slice(1);
 };
 
-const getItemStorage = (itemName) =>{
-  return JSON.parse(localStorage.getItem(itemName));
-}
+const geneateID = () => {
+	return new Date().getTime().toString(36);
+};
+
+const setItemInStorage = (itemName, item) => {
+	return localStorage.setItem(itemName, JSON.stringify(item));
+};
+
+const getItemStorage = (itemName) => {
+	return Promise.resolve().then(() => JSON.parse(localStorage.getItem(itemName)));
+};
 export default {
-    mergeStateToObjects,
-    lowerCaseFirstLetter,
-    setItemInStorage,
-    getItemStorage,
-    geneateID
-}; 
+	mergeStateToObjects,
+	lowerCaseFirstLetter,
+	setItemInStorage,
+	getItemStorage,
+	geneateID
+};
